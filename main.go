24a589ad755be91394a564351011005b2ed78a9b
Philipp Erhardt <philipperhardt29@gmail.com>
2018-12-27 09:01:28 +0100

    Crawl CCAN first so the Clonk Center crawler is able to push some items and downloads aren't delayed

--- main.go
+++ main.go
@@ -11,19 +11,19 @@ import (
 func main() {
 	var output = make(chan zipfactory.Archivable, 25)
 	go func() {
-		// Crawl an archive of clonk-center.net and return its items
-		fmt.Println("Downloading Clonk-Center items")
-		errorsCC := crawler.CrawlClonkCenter(output)
-		fmt.Printf("There were %d errors while downloading from cc-archive.lwrl.de: \n", len(errorsCC))
-		for _, err := range errorsCC {
-			fmt.Println(err.Error())
-		}
-
 		// // Crawl ccan.de and return its items
 		fmt.Println("Downloading CCAN items")
 		errorsCCAN := crawler.CrawlCCAN(output)
 		fmt.Printf("There were %d errors while downloading from ccan.de: \n", len(errorsCCAN))
 		for _, err := range errorsCCAN {
+			fmt.Println(err.Error())
+		}
+
+		// Crawl an archive of clonk-center.net and return its items
+		fmt.Println("Downloading Clonk-Center items")
+		errorsCC := crawler.CrawlClonkCenter(output)
+		fmt.Printf("There were %d errors while downloading from cc-archive.lwrl.de: \n", len(errorsCC))
+		for _, err := range errorsCC {
 			fmt.Println(err.Error())
 		}
 
